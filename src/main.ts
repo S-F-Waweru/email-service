7902feef4b5e48a02df9dbc16763701299487563
s-f-waweru <s.f.waweru@gmail.com>
2026-09-24 16:55:56 +0300

    Adde the a  a refere policy

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
           connectSrc: ["'self'", 'https://cdn.jsdelivr.net'],
         },
       },
+      referrerPolicy: { policy: 'strict-origin-when-cross-origin' }, // Explicitly set here
     }),
   );
 
